371a328e143e382e3e58358f01d0d11c66f6dbd3
aradhanakumari-git <aradhana.kumari2710@gmail.com>
2024-07-03 19:21:24 +0530

    Update Soundex.Tests.cpp
    Adding more test cases

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -5,5 +5,10 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
   generateSoundex("AX", soundex);
+  generateSoundex("HERMAN", soundex);
+  generateSoundex("HERMANN", soundex);
+  generateSoundex("MEHUL", soundex);
+  generateSoundex("MAHUL", soundex);
+
   //ASSERT_EQ(soundex,"A200");
 }
